58d5a039edbc771036d0f56bbef6d0c8b2c6e502
Andrew <kovtunets33@gmail.com>
2021-10-13 16:10:09 +0300

    Добавил лабораторную работу 5

--- Лабораторная работа 5/Лабораторная работа 5/Лабораторная работа 5.cpp
+++ Лабораторная работа 5/Лабораторная работа 5/Лабораторная работа 5.cpp
@@ -1,28 +1,43 @@
-﻿/****************************************************/
-/*              Лабораторная работа ╧5              */
-/*                 Условный оператор                */
-/*         Пример выполнение. Вариант ╧30.          */
-/****************************************************/
+﻿#define _CRT_SECURE_NO_WARNINGS
+
 #include <stdio.h>
-int main(void) 
+#include <windows.h>
+#include <iostream>
+
+using namespace std;
+
+int main(void)
 {
-    double x, y; /* координаты точки */
-     /* ввод координат */
-    printf("Введите координату x > ");
-    scanf("%lf", &x);
-    printf("Введите координату y > ");
-    scanf("%lf", &y);
-    /* вывод только что введенных значений */
-    printf("x=%6.3lf;  y=%6.3lf\n", x, y);
-    /* проверка условий */
-    if ((x >= -1 && x <= 0 && y >= -1 && y <= 0) || (x >= 0 && x <= 1 && y >= -1 && y <= 1))
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    double x, y;
+    char answer = 'y';
+
+    do
     {
-        printf("Точка попадает в область\n");
-    }
-    else
-    {
-        printf("Точка не попадает в область\n");
-    }
+        printf("Введите координату x > ");
+        scanf("%lf", &x);
+        printf("Введите координату y > ");
+        scanf("%lf", &y);
+
+        printf("x=%6.3lf;  y=%6.3lf\n\n", x, y);
+
+        printf("Ответ : ");
+        if ((x >= -1 && x <= 0 && y >= -1 && y <= 0) || (x >= 0 && x <= 1 && y >= -1 && y <= 1))
+        {
+            printf("Точка попадает в область\n\n");
+        }
+        else
+        {
+            printf("Точка не попадает в область\n\n");
+        }
+
+        printf("Хотите попробовать ввести значения еще раз (y/n) > ");
+        cin >> answer;
+        printf("\n");
+    } while (answer == 'y');
+    printf("\n\n");
 
     return 0;
 }
