89d6813ce43919838b9f89a949f6f5e53ad08a49
Andrew <kovtunets33@gmail.com>
2021-10-17 13:13:54 +0300

    Добавил лабораторные работы 5А и 5Б

---
+++ Лабораторная работа 5/5А/5А.cpp
@@ -0,0 +1,55 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <windows.h>
+#include <math.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    double a, b, c;
+    char answer = 'y';
+    do
+    {
+        printf("Введите значение коэффициента \'a\' > ");
+        int first_scan = scanf("%lf", &a);
+        printf("Введите значение коэффициента \'b\' > ");
+        int second_scan = scanf("%lf", &b);
+        printf("Введите значение коэффициента \'c\' > ");
+        int third_scan = scanf("%lf", &c);
+
+        printf("\n");
+
+        double discriminant = b * b - 4 * a * c;
+        if (discriminant >= 0)
+        {
+            double sqrt_discriminant = sqrt(discriminant);
+            double x1 = (-b + sqrt_discriminant) / (2 * a);
+            double x2 = (-b - sqrt_discriminant) / (2 * a);
+
+            if (x1 == x2)
+            {
+                printf("Заданное уравнение имеет единственное решение : x = %lf\n", x1);
+            }
+            else
+            {
+                printf("Заданное уравнение имеет два решения : x1 = %lf; x2 = %lf\n", x1, x2);
+            }
+        }
+        else
+        {
+            printf("Заданное уравнение не имеет решений\n");
+        }
+        printf("\n");
+        printf("Хотите ввести значения еще раз? (y/n) > ");
+        cin >> answer;
+        printf("\n\n");
+    } while (answer == 'y');
+
+    return 0;
+}
---
+++ Лабораторная работа 5/5Б/5Б.cpp
@@ -0,0 +1,50 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int k;
+    char answer = 'y';
+
+    do
+    {
+        printf("Введите число k > ");
+        int first_scan = scanf("%d", &k);
+
+        int number = k % 100;
+        int secondNumber = number % 10;
+
+        printf("\nРезультат : ");
+
+        if (number >= 5 && number <= 20)
+        {
+            printf("У меня %d грибов", k);
+        }
+        else if (secondNumber == 1)
+        {
+            printf("У меня %d гриб", k);
+        }
+        else if (secondNumber >= 2 && secondNumber <= 4)
+        {
+            printf("У меня %d гриба", k);
+        }
+        else
+        {
+            printf("У меня %d грибов", k);
+        }
+
+        printf("\n\n");
+        printf("Хотите ввести значение k еще раз? (y/n) > ");
+        cin >> answer;
+        printf("\n\n");
+    } while (answer == 'y');
+
+    return 0;
+}
