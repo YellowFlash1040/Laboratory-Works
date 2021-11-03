c2c031fb87e3b89e13727a50ce88d953bd9f970e
Andrew <kovtunets33@gmail.com>
2021-11-03 15:49:09 +0200

    Добавил лабы 9, 9А, 9Б

---
+++ Лабораторная работа 9/9А/9А.cpp
@@ -0,0 +1,85 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <windows.h>
+
+int main()
+{
+	SetConsoleOutputCP(1251);
+	srand(time(0));
+
+	int Matrix[9][9];
+
+	printf("Начальная матрица :\n");
+	for (int i = 0; i < 9; i++)
+	{
+		for (int j = 0; j < 8; j++)
+		{
+			Matrix[i][j] = rand() % 100;
+			printf("%3d ", Matrix[i][j]);
+		}
+		Matrix[i][8] = rand() % 100;
+		printf("%3d\n", Matrix[i][8]);
+	}
+	printf("\n\n");
+
+	int IndexOfMaxElementInString;
+	int temparage;
+	for (int i = 0; i < 9; i++)
+	{
+		IndexOfMaxElementInString = 0;
+		for (int j = 1; j < 9; j++)
+		{
+			if (Matrix[i][j] > Matrix[i][IndexOfMaxElementInString])
+			{
+				IndexOfMaxElementInString = j;
+			}
+		}
+		temparage = Matrix[i][0];
+		Matrix[i][0] = Matrix[i][IndexOfMaxElementInString];
+		Matrix[i][IndexOfMaxElementInString] = temparage;
+	}
+
+	printf("Измененная матрица (макс елемент в строке):\n");
+	for (int i = 0; i < 9; i++)
+	{
+		for (int j = 0; j < 8; j++)
+		{
+			printf("%3d ", Matrix[i][j]);
+		}
+		printf("%3d\n", Matrix[i][8]);
+	}
+	printf("\n\n");
+
+	int IndexOfMinElementInColumn;
+	int temp;
+	for (int j = 0; j < 9; j++)
+	{
+		IndexOfMinElementInColumn = 0;
+		for (int i = 1; i < 9; i++)
+		{
+			if (j == 5)
+			{
+				int a = 5;
+			}
+			if (Matrix[i][j] < Matrix[IndexOfMinElementInColumn][j])
+			{
+				IndexOfMinElementInColumn = i;
+			}
+		}
+		temp = Matrix[8][j];
+		Matrix[8][j] = Matrix[IndexOfMinElementInColumn][j];
+		Matrix[IndexOfMinElementInColumn][j] = temp;
+	}
+
+	printf("Измененная матрица (мин елемент в столбце):\n");
+	for (int i = 0; i < 9; i++)
+	{
+		for (int j = 0; j < 8; j++)
+		{
+			printf("%3d ", Matrix[i][j]);
+		}
+		printf("%3d\n", Matrix[i][8]);
+	}
+	printf("\n\n");
+}
---
+++ Лабораторная работа 9/9Б/9Б.cpp
@@ -0,0 +1,93 @@
+﻿#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+int main()
+{
+	SetConsoleOutputCP(1251);
+
+	srand(time(0));
+
+	int countOfStringsInFirstMatrix;
+	int countOfColumnsInFirstMatrix;
+
+	int countOfStringsInSecondMatrix;
+	int countOfColumnsInSecondMatrix;
+
+	do
+	{
+		cout << "Введите количество строк первой матрицы> ";
+		cin >> countOfStringsInFirstMatrix;
+		cout << "Введите количество колонок первой матрицы > ";
+		cin >> countOfColumnsInFirstMatrix;
+
+		cout << "Введите количество строк второй матрицы> ";
+		cin >> countOfStringsInSecondMatrix;
+		cout << "Введите количество колонок второй матрицы > ";
+		cin >> countOfColumnsInSecondMatrix;
+
+		if (countOfColumnsInFirstMatrix != countOfStringsInSecondMatrix)
+		{
+			cout << "\nИзвините, матрицы с такими размерами нельзя умножать. Пожалуйста введите другие размеры :\n\n";
+		}
+	} while (countOfColumnsInFirstMatrix != countOfStringsInSecondMatrix);
+	cout << "\n";
+
+	cout << "Первая матрица :\n";
+	int** FirstMatrix = new int* [countOfStringsInFirstMatrix];
+	for (int i = 0; i < countOfStringsInFirstMatrix; i++)
+	{
+		FirstMatrix[i] = new int[countOfColumnsInFirstMatrix];
+		for (int j = 0; j < countOfColumnsInFirstMatrix; j++)
+		{
+			FirstMatrix[i][j] = rand() % 11;
+			printf("%2d ", FirstMatrix[i][j]);
+		}
+		cout << endl;
+	}
+	cout << endl << endl;
+
+	cout << "Вторая матрица :\n";
+	int** SecondMatrix = new int* [countOfStringsInSecondMatrix];
+	for (int i = 0; i < countOfStringsInSecondMatrix; i++)
+	{
+		SecondMatrix[i] = new int[countOfColumnsInSecondMatrix];
+		for (int j = 0; j < countOfColumnsInSecondMatrix; j++)
+		{
+			SecondMatrix[i][j] = rand() % 11;
+			printf("%2d ", SecondMatrix[i][j]);
+		}
+		cout << endl;
+	}
+	cout << endl << endl;
+
+	
+	int** ResultMatrix = new int* [countOfColumnsInFirstMatrix];
+	for (int i = 0; i < countOfStringsInSecondMatrix; i++)
+	{
+		ResultMatrix[i] = new int[countOfStringsInSecondMatrix];
+	}
+	for (int i = 0; i < countOfColumnsInFirstMatrix; i++)
+	{
+		for (int j = 0; j < countOfStringsInSecondMatrix; j++)
+		{
+			ResultMatrix[i][j] = 0;
+		}
+	}
+
+	cout << "Матрица-результат умножения :\n";
+	for (int row = 0; row < countOfStringsInFirstMatrix; row++) 
+	{
+		for (int col = 0; col < countOfColumnsInSecondMatrix; col++) 
+		{
+			for (int inner = 0; inner < countOfColumnsInFirstMatrix; inner++) 
+			{
+				ResultMatrix[row][col] += FirstMatrix[row][inner] * SecondMatrix[inner][col];
+			}
+			cout << ResultMatrix[row][col] << " ";
+		}
+		cout << "\n";
+	}
+	cout << "\n\n";
+}
