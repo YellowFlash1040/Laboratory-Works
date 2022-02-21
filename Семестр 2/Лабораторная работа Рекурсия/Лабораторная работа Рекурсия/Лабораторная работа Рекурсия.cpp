62c16dd06f951ed7f0d62d3abc5a0fa0168d626d
Andrew <kovtunets33@gmail.com>
2022-02-21 14:31:49 +0200

    Добавил лабораторную работу с рекурсией

---
+++ Семестр 2/Лабораторная работа Рекурсия/Лабораторная работа Рекурсия/Лабораторная работа Рекурсия.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+int result;
+int ten;
+void ConvertToFifthNumberSystem(int number)
+{
+    int temp = number % 5;
+    result += temp * ten;
+    ten *= 10;
+
+    if (number/5 != 0)
+    {
+        ConvertToFifthNumberSystem(number / 5);
+    }
+}
+
+void UserInput()
+{
+    cout << "Введите число в десятичной системе счисления:\nВвод > ";
+    int answer;
+    cin >> answer;
+    cout << "\n";
+
+    ten = 1;
+    result = 0;
+    ConvertToFifthNumberSystem(answer);
+
+    cout << "Введенное вами число в пятиричной системе счисления имеет вид: " << result << "\n\n";
+    UserInput();
+}
+
+int main()
+{
+    SetConsoleOutputCP(1251);
+    UserInput();
+}
