9eca22a57a0169944044bea716b4101bbfd989ee
Andrew <kovtunets33@gmail.com>
2022-04-13 19:05:35 +0200

    Добавил лабораторную работу 3А

--- Семестр 2/Лабораторная работа 3/3А/Task3A.cpp
+++ Семестр 2/Лабораторная работа 3/3А/Task3A.cpp
@@ -4,65 +4,160 @@
 
 using namespace std;
 
-#define FirstVariant
-//#define SecondVariant
-//#define ThirdVariant
-
 // Через относительный адрес
-int* FindCountAndSumOfNegativeElementsInArray_FirstVariant(int* array, int length)
+void FindCountAndSumOfNegativeElementsInArray_FirstVariant(int* array, int length)
 {
-	int* countAndSumOfNegativeElements = new int[2];
-	*countAndSumOfNegativeElements = 0;
-	*(countAndSumOfNegativeElements + 1) = 0;
+	int count = 0;
+	int sum = 0;
 	for (int i = 0; i < length; i++)
 	{
 		if (*(array + i) < 0)
 		{
-			*countAndSumOfNegativeElements += 1;
-			*(countAndSumOfNegativeElements + 1) += *(array + i);
+			count++;
+			sum += *(array + i);
 		}
 	}
 
-	return countAndSumOfNegativeElements;
+	cout << "\nКоличество негативных елементов в  данном массиве > " << count << "\n";
+	cout << "Сумма негативных елементов данного массива > " << sum << "\n";
 }
 
 // Через абсолютный адрес
-int* FindCountAndSumOfNegativeElementsInArray_SecondVariant(int* array, int length)
+void FindCountAndSumOfNegativeElementsInArray_SecondVariant(int* array, int length)
 {
-	int* countAndSumOfNegativeElements = new int[2];
-	*countAndSumOfNegativeElements = 0;
-	*(countAndSumOfNegativeElements + 1) = 0;
+	int count = 0;
+	int sum = 0;
 	for (int i = 0; i < length; i++)
 	{
 		if (*array < 0)
 		{
-			*countAndSumOfNegativeElements += 1;
-			*(countAndSumOfNegativeElements + 1) += *array;
+			count++;
+			sum += *array;
 		}
 
 		array++;
 	}
 
-	return countAndSumOfNegativeElements;
+	cout << "\nКоличество негативных елементов в  данном массиве > " << count << "\n";
+	cout << "Сумма негативных елементов данного массива > " << sum << "\n";
 }
 
 // Без индексации
-int* FindCountAndSumOfNegativeElementsInArray_ThirdVariant(int* array, int length)
+void FindCountAndSumOfNegativeElementsInArray_ThirdVariant(int* array, int length)
 {
-	int* countAndSumOfNegativeElements = new int[2];
+	int count = 0;
+	int sum = 0;
+
 	int* array_Ptr = array;
-	*countAndSumOfNegativeElements = 0;
-	*(countAndSumOfNegativeElements + 1) = 0;
+	
 	for (; array < array_Ptr + length; array++)
 	{
 		if (*array < 0)
 		{
-			*countAndSumOfNegativeElements += 1;
-			*(countAndSumOfNegativeElements + 1) += *array;
+			count++;
+			sum += *array;
 		}
 	}
 
-	return countAndSumOfNegativeElements;
+	cout << "\nКоличество негативных елементов в  данном массиве > " << count << "\n";
+	cout << "Сумма негативных елементов данного массива > " << sum << "\n";
+}
+
+// Через массив указателей
+void FindCountAndSumOfNegativeElementsInArray_FourthVariant(int* array, int length)
+{
+	int count = 0;
+	int sum = 0;
+
+	int* array_Ptr[100];
+	
+	for (int i = 0; i < length; i++)
+	{
+		array_Ptr[i] = &array[i];
+	}
+
+	for (int i = 0; i < length; i++)
+	{
+		if (*array_Ptr[i] < 0)
+		{
+			count++;
+			sum += *array_Ptr[i];
+		}
+	}
+
+	cout << "\nКоличество негативных елементов в  данном массиве > " << count << "\n";
+	cout << "Сумма негативных елементов данного массива > " << sum << "\n";
+}
+
+// Через указатель на указатель
+void FindCountAndSumOfNegativeElementsInArray_FifthVariant(int* array, int length)
+{
+	int count = 0;
+	int sum = 0;
+
+	int* array_Ptr[100];
+
+	for (int i = 0; i < length; i++)
+	{
+		array_Ptr[i] = &array[i];
+	}
+
+	int**array_Ptr_Ptr = array_Ptr;
+
+	int** ptr = array_Ptr_Ptr;
+	for (; array_Ptr_Ptr < ptr + length; array_Ptr_Ptr++)
+	{
+		if (**array_Ptr_Ptr < 0)
+		{
+			count++;
+			sum += **array_Ptr_Ptr;
+		}
+	}
+
+	cout << "\nКоличество негативных елементов в  данном массиве > " << count << "\n";
+	cout << "Сумма негативных елементов данного массива > " << sum << "\n";
+}
+
+// В виде функции с параметрами указателями
+void FindCountAndSumOfNegativeElementsInArray_SixthVariant(int* array, int length, int* count, int* sum)
+{
+	*count = 0;
+	*sum = 0;
+	for (int i = 0; i < length; i++)
+	{
+		if (*(array + i) < 0)
+		{
+			*count += 1;
+			*sum += *(array + i);
+		}
+	}
+}
+
+void ShowVariantsOfTaskMenu()
+{
+	cout << "Выберите вариант задания: \n";
+	cout << "(1) Через относительный адрес\n";
+	cout << "(2) Через абсолютный адрес\n";
+	cout << "(3) Без индексации\n";
+	cout << "(4) Через массив указателей\n";
+	cout << "(5) Через указатель на указатель\n";
+	cout << "(6) В виде функции с параметрами указателями\n";
+	cout << "(7) Вернуться в предыдущее меню\n";
+}
+
+void ShowMainMenu()
+{
+	cout << "Выберите действие: \n";
+	cout << "(1) Выбрать вариант задания\n";
+	cout << "(2) Завершить выполнение программы\n";
+}
+
+int GetAnswerFromUser()
+{
+	int numberOfTask;
+	cout << "\nВаш ответ > ";
+	cin >> numberOfTask;
+	return numberOfTask;
 }
 
 void StartTask()
@@ -70,29 +165,67 @@ void StartTask()
 	SetConsoleOutputCP(1251);
 	srand(time(0));
 
-	int length = rand() % 9 + 2;
-	int* array = new int[length];
-
-	for (int i = 0; i < length; i++)
+	int answer;
+	do
 	{
-		*(array + i) = rand() % 21 - 10;
-		cout << *(array + i) << " ";
-	}
-	cout << "\n";
+		ShowMainMenu();
+		answer = GetAnswerFromUser();
+		cout << "\n";
 
-#ifdef FirstVariant
-	int* countAndSumOfNegativeElementsInArray = FindCountAndSumOfNegativeElementsInArray_FirstVariant(array, length);
-#endif
-#ifdef SecondVariant
-	int* countAndSumOfNegativeElementsInArray = FindCountAndSumOfNegativeElementsInArray_SecondVariant(array, length);
-#endif
-#ifdef ThirdVariant
-	int* countAndSumOfNegativeElementsInArray = FindCountAndSumOfNegativeElementsInArray_ThirdVariant(array, length);
-#endif
+		if (answer == 1)
+		{
+			int numberOfTask;
+			do
+			{
+				cout << "Сгенерированный массив: ";
+				int length = rand() % 6 + 5;
+				int* array = new int[length];
 
-	cout << "\nКоличество негативных елементов в  данном массиве > " << *countAndSumOfNegativeElementsInArray << "\n";
-	cout << "Сумма негативных елементов данного массива > " << *(countAndSumOfNegativeElementsInArray + 1) << "\n";
+				for (int i = 0; i < length; i++)
+				{
+					*(array + i) = rand() % 21 - 10;
+					cout << *(array + i) << " ";
+				}
+				cout << "\n\n";
 
-	delete[] array;
-	array = nullptr;
+				ShowVariantsOfTaskMenu();
+				numberOfTask = GetAnswerFromUser();
+
+				if (numberOfTask == 1)
+				{
+					FindCountAndSumOfNegativeElementsInArray_FirstVariant(array, length);
+				}
+				else if (numberOfTask == 2)
+				{
+					FindCountAndSumOfNegativeElementsInArray_SecondVariant(array, length);
+				}
+				else if (numberOfTask == 3)
+				{
+					FindCountAndSumOfNegativeElementsInArray_ThirdVariant(array, length);
+				}
+				else if (numberOfTask == 4)
+				{
+					FindCountAndSumOfNegativeElementsInArray_FourthVariant(array, length);
+				}
+				else if (numberOfTask == 5)
+				{
+					FindCountAndSumOfNegativeElementsInArray_FifthVariant(array, length);
+				}
+				else if (numberOfTask == 6)
+				{
+					int sum = 0;
+					int count = 0;
+					FindCountAndSumOfNegativeElementsInArray_SixthVariant(array, length, &count, &sum);
+
+					cout << "\nКоличество негативных елементов в  данном массиве > " << count << "\n";
+					cout << "Сумма негативных елементов данного массива > " << sum << "\n";
+				}
+
+				cout << "---------------------------------------------------------------\n\n";
+
+				delete[] array;
+				array = nullptr;
+			} while (numberOfTask != 7);
+		}
+	} while (answer != 2);
 }
--- Семестр 2/Лабораторная работа 3/3А/Task3A.h
+++ Семестр 2/Лабораторная работа 3/3А/Task3A.h
@@ -1,6 +1,12 @@
 #pragma once
 
-int* FindCountAndSumOfNegativeElementsInArray_FirstVariant(int* array, int length);
-int* FindCountAndSumOfNegativeElementsInArray_SecondVariant(int* array, int length);
-int* FindCountAndSumOfNegativeElementsInArray_ThirdVariant(int* array, int length);
+void FindCountAndSumOfNegativeElementsInArray_FirstVariant(int* array, int length);
+void FindCountAndSumOfNegativeElementsInArray_SecondVariant(int* array, int length);
+void FindCountAndSumOfNegativeElementsInArray_ThirdVariant(int* array, int length);
+void FindCountAndSumOfNegativeElementsInArray_FourthVariant(int* array, int length);
+void FindCountAndSumOfNegativeElementsInArray_FifthVariant(int* array, int length);
+void FindCountAndSumOfNegativeElementsInArray_SixthVariant(int* array, int length, int* count, int* sum);
+void ShowVariantsOfTaskMenu();
+int GetAnswerFromUser();
+void ShowMainMenu();
 void StartTask();
